55f072b9cbd31b02b647b4113e7f75f7c4b8c382
ntokozoprecious <ntokozotaff09@gmail.com>
2024-03-31 18:33:59 +0200

    Fixed my apiUrl

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-function handleSearchSubmit (event) {
-    event.preventDefault();
-    let searchInput = document.querySelector ("search-form-input")
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+function searchCity(city) {
+    let apiKey = "tb5d3da1bc2d33bb290609f0935457o7 ";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey} &units=metric`;
+    console.log (apiUrl);
 }
 
-let searchFormElement = document.querySelector ("search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+function handleSearchSubmit (event) {
+    event.preventDefault();
+    let searchInput = document.querySelector ("#search-form-input")
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector ("#search-form");
+searchFormElement.addEventListener("submit",handleSearchSubmit); 
+
+
+
+
+
